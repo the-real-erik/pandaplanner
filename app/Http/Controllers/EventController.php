6918061746bbb8f0df0a42776364347d42430e98
Erik <erikwernecke@gmail.com>
2022-03-12 18:59:37 -0500

    Created app

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreeventRequest;
+use App\Http\Requests\UpdateeventRequest;
+use App\Models\event;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreeventRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreeventRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\event  $event
+     * @return \Illuminate\Http\Response
+     */
+    public function show(event $event)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\event  $event
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(event $event)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateeventRequest  $request
+     * @param  \App\Models\event  $event
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateeventRequest $request, event $event)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\event  $event
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(event $event)
+    {
+        //
+    }
+}
